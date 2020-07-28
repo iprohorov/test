e78d839148b4f16c9c35521e7fdae94654e8466d
Прохоров Владимир <iprohorov2319@gmail.com>
2020-07-28 10:28:16 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "time.h"
+
+int main(void) {
+  uint32_t = 68157440;
+  for (int i = 0; i < 100; i++)
+  {
+    printf("%u\n", rand()%5);
+  }
+  return 0;
+}
